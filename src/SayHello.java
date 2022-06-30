bd3481693308601954078193b91c239453800b25
Saifur Rahman <saifurrshishir@gmail.com>
2022-07-01 01:17:41 +0600

    Exclamation added

--- src/SayHello.java
+++ src/SayHello.java
@@ -5,7 +5,7 @@ public class SayHello
 	public static void main(String[] args) 
 	{
 		// This is only to check if I can remember git command
-		System.out.println("Hello bro Anjan!");
+		System.out.println("Hello bro Anjan!!");
 	}
 
 }
