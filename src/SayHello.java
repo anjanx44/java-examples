b305cfefcddd928d8e24eb313247be71e9a89499
Saifur Rahman <saifurrshishir@gmail.com>
2022-07-01 00:34:57 +0600

    only to check if I can successfully give you a pull request

---
+++ src/SayHello.java
@@ -0,0 +1,11 @@
+
+public class SayHello 
+{
+
+	public static void main(String[] args) 
+	{
+		// This is only to check if I can remember git command
+		System.out.println("Hello bro Anjan!");
+	}
+
+}
